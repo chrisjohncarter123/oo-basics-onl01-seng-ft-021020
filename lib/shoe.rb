0cd06984bccf07dfb75ddae9e335f26033f480be
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-18 21:06:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,14 @@
 # Make your shoe class here!
 
 class Shoe
-  
+  attr_accessor :brand, :color, :size, material
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
