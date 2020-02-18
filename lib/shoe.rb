ea44998b75d156c92ea0eec9a4bcbf3e2d10219f
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-18 21:06:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,9 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :brand, :color, :size, material
-  attr_reader :title
+  attr_reader
+  attr_reader :brand, :color, :size, :material
+  attr_accessor :title
  
   def initialize(title)
     @title = title
